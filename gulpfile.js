4e748d837ee46756bbf2ad5819950f8edaeed5c5
John Papa <john@johnpapa.net>
2015-03-12 06:51:20 -0400

    added gulp-bump and yargs

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var args = require('yargs').argv;
 var config = require('./gulp.config')().getConfig();
 var del = require('del');
 var glob = require('glob');
@@ -27,6 +28,35 @@ gulp.task('vet', function() {
         .pipe($.if(env.verbose, $.print()))
         .pipe($.jshint.reporter('jshint-stylish'))
         .pipe($.jscs());
+});
+
+/**
+ * Bump the version
+ * --type=pre will bump the prerelease version *.*.*-x
+ * --type=patch or no flag will bump the patch version *.*.x
+ * --type=minor will bump the minor version *.x.*
+ * --type=major will bump the major version x.*.*
+ * --version=1.2.3 will bump to a specific version and ignore other flags
+ */
+gulp.task('bump', function() {
+    var msg = 'Bumping versions';
+    var type = args.type;
+    var version = args.ver;
+    var options = {};
+    if (version) {
+        options.version = version;
+        msg += ' to ' + version;
+    } else {
+        options.type = type;
+        msg += ' for a ' + type;
+    }
+    log(msg);
+
+    return gulp
+        .src(config.packages)
+        .pipe($.print())
+        .pipe($.bump(options))
+        .pipe(gulp.dest(config.root));
 });
 
 /**
